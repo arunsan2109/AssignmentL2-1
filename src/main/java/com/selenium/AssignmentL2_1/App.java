b040dd2906c86d7d0266ea0127ece38d41e6a778
arunsan2109 <arunsan2109@gmail.com>
2025-04-29 23:03:47 -0500

    Update App.java

--- src/main/java/com/selenium/AssignmentL2_1/App.java
+++ src/main/java/com/selenium/AssignmentL2_1/App.java
@@ -6,5 +6,6 @@ package com.selenium.AssignmentL2_1;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("New Line Added !!!");
     }
 }
