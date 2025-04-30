1eb52613f82ca62623927fc2073faf979002f048
arunsan2109 <arunsan2109@gmail.com>
2025-04-29 22:59:03 -0500

    Update App.java

--- src/main/java/com/selenium/AssignmentL2_1/App.java
+++ src/main/java/com/selenium/AssignmentL2_1/App.java
@@ -5,11 +5,6 @@ package com.selenium.AssignmentL2_1;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World! Getting the remote main updated");
-<<<<<<< HEAD
-        System.out.println("Local update");
-=======
-        System.out.println("Remote Origin update");
->>>>>>> df001cdde26318645a90dde7993908096f33cbc9
+        System.out.println("Hello World!");
     }
 }
